c9a973b00e7a733d41156f2fd013afd2068d8a13
André Rodrigues <rodrigues.domingos996@gmail.com>
2022-01-12 13:11:14 -0300

    correção das paginas

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,92 @@
+// variaveis
 var facil = document.querySelector("#facil");
+
+var confirmaPrimeiro = 'true';
+
+var confirmaSegundo = 'true';
+
+var confirmaTerceiro = 'true';
+
+// funções
+
+
 
 facil.addEventListener("click", function(){
 
-    alert("A receita é A, B, C e tem que deixar por C no fogo")
+    alert("Você escolheu a dificuldade facil, vamos lá");
 
-    var primeiro = prompt("qual o primeiro ingrediente");
+    alert("para que o Hamburger fique bom, precisamos de tres etapas, primeira vamos pegar o Pão depois temperamos a carne e deixamos na churrasqueira por 15 minutos ")
 
-    var segundo = prompt("qual o segundo ingrediente");
+    var primeiroProduto = prompt("O que devemos pegar primeiro?");
 
-    var terceiro = prompt("qual o terceiro ingrediente");
+    var primeiro = primeiroProduto.toLowerCase();
+   
+    while(true){
+        if (primeiro.length > 0 && primeiro.length < 20){
+            
+            break;
+        } else {
+            
+            alert("entrada invalida");
+            
+            primeiro = prompt("O que devemos pegar primeiro?");
+        }
+    }
+    
 
-    var quarto = prompt("quanto tempo no fogo?");
+    var segundoProduto = prompt("o que faremos com a carne?");
+
+    var segundo = segundoProduto.toLowerCase();
+
+    while(true){
+        if (segundo.length > 0 && segundo.length < 20){
+            
+            break;
+        } else {
+            
+            alert("entrada invalida");
+            
+            segundo = prompt("o que faremos com a carne?");
+        }
+    }
+
+    var terceiro = prompt("Quanto tempo na churrasqueira?");
 
 
-    var string = "aqui tem o " + primeiro + " aqui tem o " + segundo + " aqui tem o terceiro " + terceiro + " aqui tem o quarto " + quarto ;
+    while(true){
+        if (terceiro > 0 || terceiro < 60){
+            
+            break;
 
-    console.log(string)
+        } else {
+            
+            alert("entrada invalida");
+            
+            segundo = prompt("o que faremos com a carne?");
+        }
+    }
+
+
+
+    if(!primeiro == "pao" || !primeiro == "pão"){
+        
+        confirmaPrimeiro = 'false';
+
+    }
+
+    if(!segundo == "temperar" || !segundo == "tempero"){
+        confirmaSegundo = 'false';
+
+    }
+
+    if(!terceiro == 15){
+
+        confirmaTerceiro = 'false';
+
+    }
+
+
+   
+
+
 })
